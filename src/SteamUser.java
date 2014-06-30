945a8ae266d9565f042fd3355b70998e990dbc16
Saad Bakht <rotk13@gmail.com>
2014-06-30 16:50:57 -0400

    Get users inventory, but ignores items used by multiple classes

--- src/SteamUser.java
+++ src/SteamUser.java
@@ -4,18 +4,28 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Hashtable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class SteamUser {
 
-	public static void getInventory(String id) {
+	private String username;
+	
+	public SteamUser(String username) {
+		this.username = username;
+	}
+	
+	public Inventory getInventory() {
 		URL url;
-		 
+		Hashtable<String, String> items = new Hashtable<String, String>();
+		String itemClass = null;
+		String item = null;
+		
 		try {
 			// get URL content
-			url = new URL("http://www.tf2items.com/id/" + id);
+			url = new URL("http://www.tf2items.com/id/" + username);
 			URLConnection conn = url.openConnection();
  
 			// open the stream and put it into BufferedReader
@@ -35,11 +45,24 @@ public class SteamUser {
 					break;
 				}
 				
+				Matcher charClass = Pattern.compile("ubc_(.*)\"\\s").matcher(inputLine);
+				Matcher multiCharClass = Pattern.compile("ubc_(.*)\\subc_").matcher(inputLine);
+				//temporarily ignore items used by multiple classes
+				if(charClass.find() && !multiCharClass.find()) {
+//					System.out.println(charClass.group(1));
+					itemClass = charClass.group(1);
+				}
+				
 				Matcher m = p.matcher(inputLine);
 				if(m.find()) {
 					Matcher alt = Pattern.compile("alt=\"(.*)\"").matcher(inputLine);
 					if(alt.find() && !alt.group(1).contains("jewel")) {
-						System.out.println(alt.group(1));
+//						System.out.println(alt.group(1));
+//						items.add(alt.group(1));
+						item = alt.group(1);
+						items.put(itemClass, item);
+						System.out.println(itemClass);
+						System.out.println(item);
 					}
 				}
 			}
@@ -50,12 +73,9 @@ public class SteamUser {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String args[]) {
-		getInventory("iwanthotdog");
-
- 
+		
+		Inventory inventory = new Inventory(items);
+		return inventory;
 	}
 	
 }
